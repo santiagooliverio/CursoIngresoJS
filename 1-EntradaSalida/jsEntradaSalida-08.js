e8a7c962ba25351077c61becff801c820ecaaed0
santiagooliverio <oliveriosantiago006@gmail.com>
2021-07-06 21:49:09 -0300

    Avance JS 1

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -1,9 +1,22 @@
-/*
+/* Oliverio Santiago, Div H
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	alert("ok");
+	let dividendo = txtIdNumeroDividendo.value;
+	let divisor = txtIdNumeroDivisor.value;
+
+	dividendo = parseInt(dividendo);
+	divisor = parseInt(divisor);
+
+	let resto;
+	resto = dividendo % divisor;
+
+	alert("El resto es: " + resto);
 }
+
+
+// txtIdNumeroDividendo
+// txtIdNumeroDivisor
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -1,9 +1,19 @@
-/*
+/* Oliverio Santiago, Div H
 Debemos lograr tomar el importe por ID.
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	let importe;				 
+	importe = txtIdImporte.value;	
+	importe = parseInt(importe);
+
+	let resultado;
+	resultado = importe * 0.25;
+
+	txtIdResultado.value = resultado;
 }
+
+// txtIdImporte
+// txtIdResultado
